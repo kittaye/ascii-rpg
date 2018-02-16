4929961b15dd0da50bea37b5d4deb304be8aa4b3
George Delosa <georgedel1@outlook.com>
2018-02-16 21:11:33 +1000

    Re-worded a merchant log message.

--- log_messages.h
+++ log_messages.h
@@ -12,7 +12,7 @@
 
 #define LOGMSG_PLR_INTERACT_MERCHANT "Merchant: \"Greetings traveller! If you wish to trade, press ENTER.\""
 #define LOGMSG_PLR_BUY_MERCHANT "Merchant: \"Thank you for purchasing my %s!\""
-#define LOGMSG_PLR_BUY_FULL_MERCHANT "Merchant: \"Seems like you don't have any room in your inventory to buy this.\""
+#define LOGMSG_PLR_BUY_FULL_MERCHANT "Merchant: \"Seems like you don't have any room for this...\""
 #define LOGMSG_PLR_INSUFFICIENT_GOLD_MERCHANT "Merchant: \"I will not offer any lower for my goods!\""
 
 #define LOGMSG_ENEMY_DMG_PLR "%s hit Hero for %d dmg."
