b66dc19368872841968d3c19b1c08f894e110f6e
George Delosa <georgedel1@outlook.com>
2018-02-16 23:19:07 +1000

    Formatting and function signatures slightly changed.

--- george_graphics.h
+++ george_graphics.h
@@ -13,27 +13,27 @@ typedef struct GEO_Screen {
 GEO_Screen * GEO_zdk_screen;
 GEO_Screen * GEO_zdk_prev_screen;
 
-void GEO_setup_screen( void );
-void GEO_cleanup_screen( void );
-void GEO_clear_screen( void );
-void GEO_show_screen( void );
+void GEO_setup_screen(void);
+void GEO_cleanup_screen(void);
+void GEO_clear_screen(void);
+void GEO_show_screen(void);
 
-void GEO_draw_char( int x, int y, int color, char value );
-void GEO_draw_string( int x, int y, int color, char * text );
-void GEO_draw_align_center(int y, int color, char *text );
-void GEO_draw_align_right(int y, int color, char *text );
-void GEO_draw_formatted(int x, int y, int color, const char * format, ...);
-void GEO_draw_formatted_align_center(int y, int color, const char * format, ...);
-void GEO_draw_formatted_align_right(int y, int color, const char * format, ...);
-void GEO_draw_line( int x1, int y1, int x2, int y2, int color, char value );
+void GEO_draw_char(int x, int y, int color, char value);
+void GEO_draw_string(int x, int y, int color, const char *text);
+void GEO_draw_align_center(int y, int color, const char *text);
+void GEO_draw_align_right(int y, int color, const char *text);
+void GEO_draw_formatted(int x, int y, int color, const char *format, ...);
+void GEO_draw_formatted_align_center(int y, int color, const char *format, ...);
+void GEO_draw_formatted_align_right(int y, int color, const char *format, ...);
+void GEO_draw_line(int x1, int y1, int x2, int y2, int color, char value);
 
-int GEO_screen_width( void );
-int GEO_screen_height( void );
+int GEO_screen_width(void);
+int GEO_screen_height(void);
 
-int GEO_wait_char( void );
-int GEO_get_char( void );
+int GEO_wait_char(void);
+int GEO_get_char(void);
 
-void GEO_override_screen_size( int width, int height );
-void GEO_fit_screen_to_window( void );
+void GEO_override_screen_size(int width, int height);
+void GEO_fit_screen_to_window(void);
 
 #endif /* GRAPHICS_H_ */
