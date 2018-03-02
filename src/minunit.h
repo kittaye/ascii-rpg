87929a3826f4baf78c7d2a3ff6832144930d884e
George Delosa <georgedel1@outlook.com>
2018-03-02 21:45:49 +1000

    Modified implementation of MinUnit for better unit testing functionality

--- src/minunit.h
+++ src/minunit.h
@@ -3,12 +3,14 @@
 // *	License (from source): "You may use the code in this tech note for any purpose, with the understanding that it comes with NO WARRANTY."
 // *
 // *	Sourced from: http://www.jera.com/techinfo/jtns/jtn002.html
+// *	Modified to use a global buffer to contain the failed test's function name, assertion expression, and line number.
 // *
 #ifndef MINUNIT_H_
 #define MINUNIT_H_
 
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
+#define mu_assert(func, test) do { if (!(test)) { snprintf(G_TEST_FAILED_BUF, 1024, "%s(): Line %d: assertion (%s) failed", func, __LINE__, #test); return 1; } } while (0)
+#define mu_run_test(test) do { int result = test(); tests_run++; if (result) return result; } while (0)
+char G_TEST_FAILED_BUF[1024];
 extern int tests_run;
 
 #endif // !MINUNIT_H_
