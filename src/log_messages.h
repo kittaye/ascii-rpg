b22e90639452bc5e6dc50f31c7c6c891e885bd0d
George Delosa <georgedel1@outlook.com>
2018-03-06 16:59:47 +1000

    Added defines for dropping / examining items

--- src/log_messages.h
+++ src/log_messages.h
@@ -7,11 +7,16 @@
 #define LOGMSG_PLR_USE_FOOD_FULL "You're full, but you ate the food anyway."
 #define LOGMSG_PLR_USE_MAP "The map disintegrates as it reveals the floor's layout to you."
 #define LOGMSG_PLR_GET_ITEM "You picked up %s."
+#define LOGMSG_PLR_DROP_ITEM "You dropped %s."
+#define LOGMSG_PLR_CANT_DROP_ITEM "There is not enough space to drop this item here."
 #define LOGMSG_PLR_INVENTORY_FULL "Your inventory is too full to pick up any more items."
 #define LOGMSG_PLR_DMG_ENEMY "You hit %s for %d dmg."
 #define LOGMSG_PLR_KILL_ENEMY "You have slain %s."
 #define LOGMSG_PLR_INTERACT_STAIRCASE "You ascend the staircase..."
 #define LOGMSG_PLR_NEW_FLOOR "You are now traversing floor %d."
+
+#define LOGMSG_EXAMINE_SMALL_FOOD "An oddly clean-looking assortment of fruits."
+#define LOGMSG_EXAMINE_BIG_FOOD "A freshly cooked meal."
 
 #define LOGMSG_PLR_INTERACT_MERCHANT "Merchant: \"Greetings traveller! If you wish to trade, press ENTER.\""
 #define LOGMSG_PLR_BUY_MERCHANT "Merchant: \"Thank you for purchasing my %s!\""
