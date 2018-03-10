425dc870ec2a109753827a3d64611581fe31b7bc
George Delosa <georgedel1@outlook.com>
2018-03-10 20:32:36 +1000

    Formatting changes to the database

--- src/items.c
+++ src/items.c
@@ -3,9 +3,9 @@
 #define SPR_EMPTY ' '
 
 static const item_t g_item_database[] = {
-	[ItmSlug_NONE] = {.name = "Empty", .sprite = SPR_EMPTY, .item_slug = ItmSlug_NONE, .value = 0},
-	[ItmSlug_SMALLFOOD] = {.name = "Small food", .sprite = SPR_SMALLFOOD, .item_slug = ItmSlug_SMALLFOOD, .value = 20},
-	[ItmSlug_BIGFOOD] = {.name = "Big food", .sprite = SPR_BIGFOOD, .item_slug = ItmSlug_BIGFOOD, .value = 35}
+	[ItmSlug_NONE] =		{.name = "Empty",		.sprite = SPR_EMPTY,		.item_slug = ItmSlug_NONE,		.value = 0},
+	[ItmSlug_SMALLFOOD] =	{.name = "Small food",	.sprite = SPR_SMALLFOOD,	.item_slug = ItmSlug_SMALLFOOD, .value = 20},
+	[ItmSlug_BIGFOOD] =		{.name = "Big food",	.sprite = SPR_BIGFOOD,		.item_slug = ItmSlug_BIGFOOD,	.value = 35}
 };
 
 const item_t* Get_Item(const item_slug_en item_slug) {
