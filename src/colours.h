406982ef24cd89c29f48f332530fd25d628020be
George Delosa <georgedel1@outlook.com>
2018-03-10 12:18:14 +1000

    Added seperate header for the colours enum

---
+++ src/colours.h
@@ -0,0 +1,14 @@
+#ifndef COLOURS_H_
+#define COLOURS_H_
+
+typedef enum colour_en {
+	Clr_WHITE = 0,
+	Clr_YELLOW = 1,
+	Clr_RED = 2,
+	Clr_BLUE = 3,
+	Clr_MAGENTA = 4,
+	Clr_CYAN = 5,
+	Clr_GREEN = 6
+} colour_en;
+
+#endif /* COLOURS_H_ */
