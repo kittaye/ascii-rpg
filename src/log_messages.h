eb25b923644d499d6d1371362e9e3b1ef611827d
George Delosa <georgedel1@outlook.com>
2018-03-11 13:52:53 +1000

    Updated log messages

--- src/log_messages.h
+++ src/log_messages.h
@@ -1,16 +1,16 @@
 #ifndef LOG_MESSAGES_H_
 #define LOG_MESSAGES_H_
 
-#define LOGMSG_PLR_GET_GOLD_SINGLE "You picked up %d gold piece."
-#define LOGMSG_PLR_GET_GOLD_PLURAL "You picked up %d gold pieces."
+#define LOGMSG_PLR_GET_GOLD_SINGLE "Picked up %d gold piece."
+#define LOGMSG_PLR_GET_GOLD_PLURAL "Picked up %d gold pieces."
 #define LOGMSG_PLR_USE_FOOD "You restored %d health from eating food."
-#define LOGMSG_PLR_USE_FOOD_FULL "You're full, but you ate the food anyway."
-#define LOGMSG_PLR_GET_ITEM "You picked up %s."
-#define LOGMSG_PLR_DROP_ITEM "You dropped %s."
+#define LOGMSG_PLR_USE_FOOD_FULL "You're full, but ate the food anyway."
+#define LOGMSG_PLR_GET_ITEM "Picked up %s."
+#define LOGMSG_PLR_DROP_ITEM "Dropped %s."
 #define LOGMSG_PLR_CANT_DROP_ITEM "There is not enough space to drop this item here."
 #define LOGMSG_PLR_INVENTORY_FULL "Your inventory is too full to pick up any more items."
-#define LOGMSG_PLR_DMG_ENEMY "You hit %s for %d dmg."
-#define LOGMSG_PLR_KILL_ENEMY "You have slain %s."
+#define LOGMSG_PLR_DMG_ENEMY "You hit the %s for %d dmg."
+#define LOGMSG_PLR_KILL_ENEMY "You have slain the %s."
 #define LOGMSG_PLR_INTERACT_STAIRCASE "You ascend the staircase..."
 #define LOGMSG_PLR_NEW_FLOOR "You are now traversing floor %d."
 
@@ -22,7 +22,7 @@
 #define LOGMSG_PLR_BUY_FULL_MERCHANT "Merchant: \"Seems like you don't have any room for this...\""
 #define LOGMSG_PLR_INSUFFICIENT_GOLD_MERCHANT "Merchant: \"I won't offer any lower for this item!\""
 
-#define LOGMSG_ENEMY_DMG_PLR "%s hit you for %d dmg."
+#define LOGMSG_ENEMY_DMG_PLR "The %s hits you for %d dmg."
 
 #define LOGMSG_WELCOME "Welcome to Asciiscape!"
 
