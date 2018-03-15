e0def7418ece5fe4e7780cc75e485992fa741b40
George Delosa <georgedel1@outlook.com>
2018-03-15 17:24:51 +1000

    Reverted player moving into void tiles to be allowed again

--- src/ascii_game.c
+++ src/ascii_game.c
@@ -327,7 +327,7 @@ player_t Create_Player(void) {
 bool Try_SetPlayerPos(game_state_t *state, coord_t pos) {
 	assert(state != NULL);
 
-	if (!Check_OutOfWorldBounds(pos) && state->world_tiles[pos.x][pos.y].data->type != TileType_SOLID && state->world_tiles[pos.x][pos.y].data->type != TileType_VOID) {
+	if (!Check_OutOfWorldBounds(pos) && state->world_tiles[pos.x][pos.y].data->type != TileType_SOLID) {
 		state->player.pos = pos;
 		return true;
 	}
