08cf9733c273caed3c0d94af0a2c8eaa0ae26f18
George Delosa <georgedel1@outlook.com>
2018-04-16 17:31:24 +1000

    Updated merchant messages

--- src/log_messages.h
+++ src/log_messages.h
@@ -15,8 +15,8 @@
 
 #define LOGMSG_PLR_TALK_MERCHANT "Merchant: \"Greetings traveller! If you wish to trade, press ENTER.\""
 #define LOGMSG_PLR_BUY_MERCHANT "Merchant: \"Thank you for purchasing my %s!\""
-#define LOGMSG_PLR_BUY_FULL_MERCHANT "Merchant: \"Seems like you don't have any room for this...\""
-#define LOGMSG_PLR_INSUFFICIENT_GOLD_MERCHANT "Merchant: \"I won't offer any lower for this item!\""
+#define LOGMSG_PLR_BUY_FULL_MERCHANT "Merchant: \"Seems like you don't have any room to hold this...\""
+#define LOGMSG_PLR_INSUFFICIENT_GOLD_MERCHANT "Merchant: \"I won't offer any lower for this %s!\""
 
 #define LOGMSG_ENEMY_DMG_PLR "The %s hits you for %d dmg."
 
